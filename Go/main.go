d0ea810d304d59d2773bfe74cb98cc40277932f8
ubuntu <bill@schmeister.com>
2022-04-13 11:46:49 -0700

    sub folders

--- Go/main.go
+++ Go/main.go
@@ -1,42 +1,7 @@
 package main
 
-import "fmt"
-
-type Node struct {
-	data     int
-	children []*Node
-}
-
-func (root *Node) addChild(i int) {
-	n := Node{
-		data:     i,
-		children: []*Node{},
-	}
-	root.children = append(root.children, &n)
-}
-
-func (root *Node) preOrder() {
-	if root == nil {
-		return
-	}
-	fmt.Println(root.data)
-
-	for _, node := range root.children {
-		node.preOrder()
-	}
-}
+import "github.com/schmeister/codingPrep/trees"
 
 func main() {
-	root := &Node{
-		data:     10,
-		children: []*Node{},
-	}
-	root.addChild(8)
-	root.addChild(5)
-
-	root.children[0].addChild(2)
-	root.children[0].addChild(1)
-	root.children[0].addChild(6)
-
-	root.preOrder()
+	trees.Nary()
 }
